a1b5cf6645695408657fff5b6c4de979f05f0c9c
Alexandru Stoican <148752066+AlexSt3002@users.noreply.github.com>
2024-11-05 19:07:29 +0000

    beta3

--- app.py
+++ app.py
@@ -6,16 +6,77 @@ def load_medici():
   with engine.connect() as conn:
     result = conn.execute(text("SELECT * FROM Medici"))
     result_all=[]
-    rows = result.fetchall()  # preia toate rezultatele
+    rows = result.fetchall()  
+    for row in rows:
+      result_all.append(row)
+    return result_all
+
+def load_medicamente():
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM Medicamente"))
+    result_all=[]
+    rows = result.fetchall()  
+    for row in rows:
+      result_all.append(row)
+    return result_all
+
+def load_pacienti():
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM Pacienti"))
+    result_all=[]
+    rows = result.fetchall()  
+    for row in rows:
+      result_all.append(row)
+    return result_all
+
+def load_studii():
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM Studii_Clinice"))
+    result_all=[]
+    rows = result.fetchall()  
+    for row in rows:
+      result_all.append(row)
+    return result_all
+
+def load_locatie():
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM Locatie"))
+    result_all=[]
+    rows = result.fetchall()  
     for row in rows:
       result_all.append(row)
     return result_all
       
 @app.route('/')
 def hello_world():
+  return render_template('home.html')
+
+@app.route('/dashboard')
+def dashboard():
+  return render_template('dashboard.html')
+
+@app.route('/medici')
+def medici():
   medici=load_medici()
-  return render_template('home.html',medici=medici)
+  return render_template('medici.html',medici=medici)
+@app.route('/medicamente')
+def medicamente():
+  medicamente=load_medicamente()
+  return render_template('medicamente.html',medicamente=medicamente)
 
+@app.route('/pacienti')
+def pacienti():
+  pacienti=load_pacienti()
+  return render_template('pacienti.html',pacienti=pacienti)
 
+@app.route('/studii')
+def studii():
+  studii=load_studii()
+  return render_template('studii.html',studii=studii)
+
+@app.route('/locatie')
+def locatie():
+  locatie=load_locatie()
+  return render_template('locatie.html',locatie=locatie)
 if __name__=='__main__':
   app.run(host='0.0.0.0',debug=True)
